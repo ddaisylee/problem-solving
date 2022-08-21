692d39d249cf6b2cdb9a454446d6591af719d7f5
ddaisylee <elliejay9988@gmail.com>
2022-08-22 08:19:47 +0900

    Create: 백준 큐 1문제
    Chore: 백준 스택 문제 리팩토링 및 주석 삭제

--- baekjoon/큐/1_큐/index.js
+++ baekjoon/큐/1_큐/index.js
@@ -1,31 +1,29 @@
 const arr = require('fs').readFileSync('./input.txt').toString().trim().split('\n');
 const n = arr.shift();
 
-const stack = [];
+const queue = [];
 const result = [];
 
 function solution(n, arr) {
   for (let i = 0; i < n; i++) {
     switch (arr[i]) {
       case 'pop':
-        result.push(stack.pop() || -1);
+        result.push(queue.shift() || -1);
         break;
-
       case 'size':
-        result.push(stack.length);
+        result.push(queue.length);
         break;
-
       case 'empty':
-        result.push(stack[0] ? 0 : 1);
+        result.push(!queue.length ? 1 : 0);
         break;
-
-      case 'top':
-        result.push(stack[stack.length - 1] || -1);
+      case 'front':
+        result.push(queue[0] || -1);
         break;
-
+      case 'back':
+        result.push(queue[queue.length - 1] || -1);
+        break;
       default:
-        stack.push(arr[i].split(" ")[1]);
-        break;
+        queue.push(arr[i].split(' ')[1]);
     }
   }
   console.log(result.join('\n'));
