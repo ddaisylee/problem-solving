a421c943acda83b290124254e060b52328a9f27d
Eunji Lee <88873956+ddaisylee@users.noreply.github.com>
2022-10-21 14:45:50 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
+++ 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
@@ -1,23 +1,30 @@
-function solution(n, m) {
-    let a = Math.max(n,m);
-    let b = Math.min(n,m);
+// function solution(n, m) {
+//     let a = Math.max(n,m);
+//     let b = Math.min(n,m);
     
-    // 최대공약수
-    function gcd(a, b){
-        let remainder;
-        while(a % b > 0) {
-            remainder = a % b;
-            a = b;
-            b = remainder;
-        }
-        return b;
-    }
+//     // 최대공약수
+//     function gcd(a, b){
+//         let remainder;
+//         while(a % b > 0) {
+//             remainder = a % b;
+//             a = b;
+//             b = remainder;
+//         }
+//         return b;
+//     }
     
-    // 최소공배수 
-    function lcm(a, b){
-        let k = gcd(a, b);
-        return (a * b) / k
-    }
+//     // 최소공배수 
+//     function lcm(a, b){
+//         let k = gcd(a, b);
+//         return (a * b) / k
+//     }
     
-    return [gcd(a, b), lcm(a, b)]
+//     return [gcd(a, b), lcm(a, b)]
+// }
+
+
+function solution(a, b) {
+    var r;
+    for(var ab= a*b;r = a % b;a = b, b = r){}
+    return [b, ab/b];
 }
