15e1d4d6f238a28d79f708642d9912855770fc69
Eunji Lee <88873956+ddaisylee@users.noreply.github.com>
2022-12-14 22:21:04 +0900

    [unrated] Title: 가장 가까운 같은 글자, Time: 5.21 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -1,15 +1,26 @@
-function solution(s) {
-    const arr = [];
-    const elements = s.split('');
+// function solution(s) {
+//     const arr = [];
+//     const elements = s.split('');
 
-    for(let i = 0; i < s.length; i++){
-        const sliced = elements.slice(0, i);
-        if(sliced.includes(s[i])) {
-            let idxOfSame = sliced.lastIndexOf(s[i])
-            arr.push(i - idxOfSame)
-        } else {
-            arr.push(-1);   
-        }
-    }
-    return arr;
+//     for(let i = 0; i < s.length; i++){
+//         const sliced = elements.slice(0, i);
+//         if(sliced.includes(s[i])) {
+//             let idxOfSame = sliced.lastIndexOf(s[i])
+//             arr.push(i - idxOfSame)
+//         } else {
+//             arr.push(-1);   
+//         }
+//     }
+//     return arr;
+// }
+
+function solution(s) {
+  const obj = {};
+  const answer = [];
+  for (let i = 0; i < s.length; i++) {
+    if (obj[s[i]] != null) answer[i] = i - obj[s[i]];
+    else answer[i] = -1;
+    obj[s[i]] = i;
+  }
+  return answer;
 }
