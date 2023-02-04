7af0549d019b6a555c77cc9e0a91a20504689fa9
Eunji Lee <88873956+ddaisylee@users.noreply.github.com>
2023-02-04 16:05:10 +0900

    [level 2] Title: 예상 대진표, Time: 0.23 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.js
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.js
@@ -1,20 +1,18 @@
-function solution(n,a,b){  
-    const isPair = (numA, numB) => {
-        let isOdd = Math.min(numA, numB) % 2 === 1;
-        let difference = Math.abs(numA - numB) === 1;
+function solution(n,a,b){ 
+    const isPair = (a, b) => {
+        let isOdd = Math.min(a, b) % 2 === 1;
+        let difference = Math.abs(a - b) === 1;
         return isOdd && difference;
     }
     
     let round = 1;
     
-    while(round < n / 2) {
-        if(isPair(a, b)) {
-            return round;
-        } else {            
+    while(a !== b) {
+        if(isPair(a, b)) break;
+        
             a = Math.ceil(a / 2);
             b = Math.ceil(b / 2);
             round++;
         }
-    }
     return round;
 }
